0ec2f016d2afac8caa9f8f56c5d0116ea3833acb
Jakub Mikulas <jakub@jedenbod.cz>
2012-08-03 14:50:18 +0200

    Vypis kampani
    CSS, uprava UI, vypis info o klientovi

--- welcome.php
+++ welcome.php
@@ -1,25 +1,82 @@
 <?php include ('inc/header.php') ?>
 
-<?php 
+<div id="header">
+  <img src="img/sklik_logo.png" />
+    <div id="klient">  
+    <?php
+    
+    $klient = volej("client.getAttributes");
+    
+    echo $klient["user"]["username"]."<br />";
+    $kreditTecka = $klient["user"]["walletCredit"]/100;
+    $kredit=str_replace(".", ",", $kreditTecka);
+    echo $kredit." Kè";
+    ?>    
+    </div>
+  
+  
+  
+  
+  <img id="logout" src="img/logout.png" />
+</div>
+<?php
+
+
+ 
     $kampane = volej("listCampaigns");
-    // print_r($odpoved);
-    foreach ($kampane["campaigns"] as $kampan) {     
+    // print_r($kampane);
+    
+    foreach ($kampane["campaigns"] as $kampan) {   
+    
+    $rozpocet = $kampan["dayBudget"]/100;
+    $status = $kampan["status"];
+    
+              
+        echo      
+        '<div class="kampan '.$status.'">
+          <div class="inside">
+          <h2>'.$kampan["name"].'</h2>
+          <div class="kampanData">
+          
+            <div class="jednaTri">
+              Prokliky
+              <strong>196</strong>
+            </div>
             
-      echo '<div data-role="button">
-         <h1>'.$kampan["name"].'</h1>
-         <div class="ui-grid-b">
-         <div class="ui-block-a">CTR</div>
-         <div class="ui-block-b">CTR</div>
-         <div class="ui-block-c">CTR</div>
-         </div>
-         <div class="ui-grid-b">
-         <div class="ui-block-a">CTR</div>
-         <div class="ui-block-b">CTR</div>
-         <div class="ui-block-c">CTR</div>
-         </div>   
-      </div>';                  
+            <div class="jednaTri">
+              Zobrazení
+              <strong>136 657</strong>
+            </div>
+            
+            <div class="jednaTri">
+              CTR
+              <strong>1,68%</strong>
+            </div>
+                      
+           
+            <div class="jednaTri">
+              Denní rozpoèet
+              <strong>'.$rozpocet.'</strong>
+            </div>
+            
+            <div class="jednaTri">
+              Prùmìrná CPC
+              <strong>9,68</strong>
+            </div>
+            
+            <div class="jednaTri">
+              Cena
+              <strong>1 987,44</strong>
+            </div>                    
+          
+            <br class="clear" />
+          
+          </div>
+          </div>
+        </div>';                         
       
     };
-?>
+       
+?>             
 
 <?php include ('inc/footer.php') ?>
