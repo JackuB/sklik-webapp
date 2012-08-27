4f9a6a590d1838c8c50cf2fb050066186092c1e9
Jakub Mikulas <jakub@jedenbod.cz>
2012-08-28 01:00:07 +0200

    Mobyklik feature web

--- WEB/index.php
+++ WEB/index.php
@@ -19,37 +19,42 @@ if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),"iphone") or strpos(strtolowe
 </div>
 
 	<div id="wrap">
-  
-    <div class="fialova">
-      <h2>Mobyklik je mobilní rozhraní pro <a href="http://sklik.cz">Sklik.cz</a></h2>
+         <img src="img/mobyklik.jpg" alt="mobyklik - sklik.cz pro iPhone, Android, iPad" />
+         
+         
+
+      <h2>Mobyklik je <s style="color:#ccc">mobilní</s> rozhraní pro <a href="http://sklik.cz">Sklik.cz</a></h2>
       
-      <p>Vytváøí ho <a href="http://jakub.jedenbod.cz/">Jakub Mikuláš</a> jako <a href="https://github.com/JackuB/sklik-webapp">open-source</a></p>
-      
-      <p>Využívá <a href="http://api.sklik.cz/">Sklik API</a></p>
+      <p>Vytváøí ho <a href="http://jakub.jedenbod.cz/">Jakub Mikuláš</a> jako <a href="https://github.com/JackuB/sklik-webapp">open-source</a>. Využívá <a href="http://api.sklik.cz/">Sklik API</a>.</p>
       
       <p>Hlavním cílem je pøinést pøíjemné rozhraní</p>
       
-      <p>Prozatím podporuje jen iOS, ale mùžete mi <a href="http://jakub.jedenbod.cz/">napsat</a>, Android musím nejdøíve otestovat.</p>
+      <a href="app/" class="button">
+        Spustit Mobyklik              
+      </a><small style="margin-bottom:35px;display:block;">Nebo navštivte tento web na svém telefonu</small>
+      
+      <h2>Co funguje?</h2>
+      <ul>
+        <li>Výpis kampaní, reklam a klíèových slov</li>
+        <li><strong>Pøístup pøes telefony</strong> <em>(iPhone, Android)</em>, <strong>tablety</strong> <em>(iPad, Android)</em> a <strong>desktopy</strong> <em>(Win, OS X i Linux)</em></strong></li>
+        <li>Poèítání CTR, CPC</li>
+        <li><strong>Grafy v Skliku</strong></li>
+        <li>Detekce aktivních a neaktivních kampaní</li> 
+      </ul>
         
       <h2>Co je v plánu?</h2>
       
       <ul>
-        <li>Výpis reklam</li>
-        <li>Podpora Androidu</li>
-        <li>Podpora Windows Phone 7</li>
-        <li>Urychlení pøes <em>system.multicall</em></li>
+        <li>Urychlení (pøes <em>system.multicall</em>)</li>
+        <li>Øazení a filtrování dat</li>
         <li>Editace dat</li>
         <li>Vkládání dat</li>      
         <li>Uživatelská nastavení</li>
-        <li>Verze pro desktopové prohlížeèe</li>
+        <li>Automatická tvorba pøehledù</li>
+        <li>Sledování výkonnosti reklam</li>
         <li>Desktopová app</li>
         <li>...</li>  
       </ul>
-    </div>
-    
-    <div class="screenshot">
-      <img src="img/mobyklik.jpg" alt="Mobilní aplikace pro Sklik.cz" />
-    </div>
 		
 	 </div>	
 
